123b010479dde01d0feb36ff8c4a470277695e59
2405000879 <fredshem73@gmail.com>
2025-04-12 20:58:16 +0200

    Add files via upload

---
+++ srings.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>My First Webpage</title>
+        </head>
+        <body>
+            <h1>My First dyanamic page</h1>
+            <?php 
+
+            $name="Shema";
+            $course="BBIT";
+            ?>
+            <p> My name is <?php echo $name; ?> and I am studying <?php echo $course; ?></p> 
+            </body>
+
+            </html>
